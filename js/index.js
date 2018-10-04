11424791b8a92e57d4683e0a95966f74a5a9bf2b
Jacek Walasik <jacekwalasik89@gmail.com>
2018-10-04 16:52:19 +0200

    star body

--- js/index.js
+++ js/index.js
@@ -109,7 +109,7 @@ function init(){
     var delta = clock.getDelta();
     trackballControls.update(delta);
     uniform.time.value += 0.02;
-    surfaceUniform.time.value +=0.02;
+    surfaceUniform.time.value +=0.5;
     checkHighlight();
 
     requestAnimationFrame(render);
