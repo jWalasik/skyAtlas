452674c512b328a2e876b8650de1789cbaf4e639
Jacek Walasik <jacekwalasik89@gmail.com>
2018-11-15 21:48:00 +0100

    rotation fix

--- js/return.js
+++ js/return.js
@@ -1,7 +1,9 @@
 var goBack = function(){
   if(lvl == 2){
-    camera.position=prevPos
-    camera.rotation=prevRot  
+    camera.position.set(prevPos.x, prevPos.y,prevPos.z)
+    camera.rotation=prevRot
+    trackballControls.update();
+    console.log('current pos: ', camera.position, 'stored pos: ', prevPos)
 	  lvl = 1;
     document.getElementById('name-container').innerHTML = "";
 	  document.getElementById('description-container').innerHTML = "";
@@ -10,8 +12,10 @@ var goBack = function(){
     
   }
   else if( lvl == 3){
-    camera.position=prevPos
-    camera.rotation=prevRot 
+    camera.position.set(prevPos.x, prevPos.y,prevPos.z)
+    camera.rotation=prevRot
+    console.log('current pos: ', camera.position, 'stored pos: ', prevPos)
+    trackballControls.update();
     lvl = 2;
   	document.getElementById('name-container').innerHTML = name;
   	document.getElementById('description-container').innerHTML = name;
